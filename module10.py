2b3decde5940af6301f8fa5c0509cf1cc1cd673a
csalerno1 <77583966+csalerno1@users.noreply.github.com>
2021-03-21 21:51:08 -0400

    Create module10.py

---
+++ module10.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+fig, figure = plt.subplots()
+
+data = {
+	"Hamburger": 139,
+	"Hotdog": 216,
+	"Cheeseburger": 182,
+	"Chili Cheese Dog": 57
+}
+
+x_axis_data = data.keys()
+y_axis_data = data.values()
+
+figure.bar(x_axis_data, y_axis_data)
+
+figure.set(ylim=[0,250],
+	ylabel= 'Number of Orders',
+	xlabel = 'Menu Item',
+	title = 'Number of Orders for each Menu Item at the Baseball Game')
+
+plt.show()
+
+import numpy as np
+import pandas as pd
+from plotnine import *
+scores = pd.read_csv('C://Users//caitl//Documents//scores.csv')
+ggplot(scores, aes(x='Hours_Studied',y='Score_On_Exam'))+geom_point()
